c06de78e655a5382150db16657108a9122f44104
Lu <Lucinan@nycstudents.net>
2026-09-18 10:02:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import turtle
-from turtle import
-t = Turtle ()
-
-t.shape('turtle')
-t.forward(200)
-
-
-
-turtle.done()
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
