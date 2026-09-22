40e46dacc691222d78281d07a100b35f0635462b
Lu <Lucinan@nycstudents.net>
2026-09-22 10:16:36 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+""" age = 66
+if age > 65:
+    print("Senior citizen.")
+elif age > 18:
+    print("Adult.")
+else:
+    print("Child.") """
+""" has_key = False
+is_magic = True
+if has_key and is_magic:
+    print("You can open the door.")
+else:
+    print("Enter castle")
+else:
+    print("Cannot enter") """
