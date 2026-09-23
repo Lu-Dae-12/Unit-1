803eb499b8d1f6f7767458fd7b4d7373ea4fc1af
Lu <Lucinan@nycstudents.net>
2026-09-23 10:09:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-import turtle
-from turtle import *
-t = Turtle()
-def square(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
+#integer
+x = 10
+#float
+y = 3.14
+#boolean
+z = True
+#string
+name "steve"
+#lists
+students = ["Natalie", "Martin", "Ben", "Cedar"]
+students.append("Steve")
+print(students[5])
